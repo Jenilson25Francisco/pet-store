0e483ac9f8b093d0593c29036710bb6b187390c0
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-03-25 19:20:39 +0100

    feat: adding user controller

---
+++ src/main/java/com/zanguetsuinc/petstoreapi/api/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.zanguetsuinc.petstoreapi.api.controller;
+
+import com.zanguetsuinc.petstoreapi.api.assembler.UserAssembler;
+import com.zanguetsuinc.petstoreapi.api.dto.UserDto;
+import com.zanguetsuinc.petstoreapi.api.response.UserResponse;
+import com.zanguetsuinc.petstoreapi.domain.model.User;
+import com.zanguetsuinc.petstoreapi.domain.repository.UserRepository;
+import com.zanguetsuinc.petstoreapi.domain.service.CreateUserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/user")
+@AllArgsConstructor
+public class UserController {
+
+    private UserRepository userRepository;
+    private UserAssembler userAssembler;
+    private CreateUserService createUserService;
+
+    @GetMapping
+    public List<UserDto> listar(){
+        return userAssembler.toCollectionModel(userRepository.findAll());
+    }
+
+    @GetMapping("{userId}")
+    public ResponseEntity<UserDto> getUser(@PathVariable UUID userId){
+        return userRepository.findById(userId)
+                .map(user -> ResponseEntity.ok(userAssembler.toDto(user)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto criar(@Valid @RequestBody UserResponse userResponse){
+        User newUser = userAssembler.toResponse(userResponse);
+        User saveUser = createUserService.create(newUser);
+
+        return userAssembler.toDto(saveUser);
+    }
+
+}
