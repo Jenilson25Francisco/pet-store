b722c8115d4307dd8cd8bb769dd8d6b11532d1f3
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-03-26 13:13:50 +0100

    feat: adding pet entity

---
+++ src/main/java/com/zanguetsuinc/petstoreapi/domain/model/Pet.java
@@ -0,0 +1,28 @@
+package com.zanguetsuinc.petstoreapi.domain.model;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Pet {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String raca;
+    private String cor;
+    private String sexo;
+    private int idade;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
