0d4284c159c482224f859db748f86a67feea82dd
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-03-26 17:21:38 +0100

    feat: adding pet dto

---
+++ src/main/java/com/zanguetsuinc/petstoreapi/api/dto/PetDto.java
@@ -0,0 +1,20 @@
+package com.zanguetsuinc.petstoreapi.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class PetDto {
+
+    private UUID id;
+    private String raca;
+    private String cor;
+    private String sexo;
+    private String imageUrl;
+    private int idade;
+    private ResumeUserDto user;
+
+}
