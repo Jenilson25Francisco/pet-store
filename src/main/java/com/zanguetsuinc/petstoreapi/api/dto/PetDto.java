c39ebf2a14f18b4cf29260fdb8451c28cb7c2ca3
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-03-29 14:54:49 +0100

    fix: adding price field on pet dto

--- src/main/java/com/zanguetsuinc/petstoreapi/api/dto/PetDto.java
+++ src/main/java/com/zanguetsuinc/petstoreapi/api/dto/PetDto.java
@@ -3,6 +3,7 @@ package com.zanguetsuinc.petstoreapi.api.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Getter
@@ -15,6 +16,7 @@ public class PetDto {
     private String sexo;
     private String imageUrl;
     private int idade;
+    private BigDecimal preco;
     private ResumeUserDto user;
 
 }
